e6dd4f9be9c0561a8319ce0cd4e18190342f89c9
Simon Caine <simon@offshoreweather.com.au>
2018-03-21 02:02:01 +0000

    almost lost this, updating and pushing to bitbucket

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup
 
-setup(name='adjustText',
+setup(name='adjustText_simon',
       version='0.6.1',
       description='Iteratively adjust text position in matplotlib plots to minimize overlaps',
       author='Ilya Flyamer',
       author_email='flyamer@gmail.com',
       url='https://github.com/Phlya/adjustText',
-      packages=['adjustText'],
+      packages=['adjustText_simon'],
       install_requires=['numpy', 'matplotlib']
      )
